c970987c91460ff57832759e62c50c22a9fd0b58
charliesgithub20221030 <charliesmail20221030@gmail.com>
2019-06-17 16:59:27 +0800

    new v

--- sa_final/settings.py
+++ sa_final/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'front', 'static'),
+    os.path.join(BASE_DIR, 'fa_system', 'static'),
 ]
 PROJECT_ROOT = BASE_DIR
 
